a99b4e6b82ef93d8cf50bcd8f6800f670eda76d6
arai <aabhash@pegotec.net>
2022-12-01 23:55:28 +0545

    app listened after server connected

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL).then(()=>{
     console.log("DB connection successfull");
+    app.listen(process.env.PORT 
+        || 8000, ()=>{
+        console.log("Backend server is running");
+    });
+    
 }).catch((err)=>{
     console.log(err);
 });
@@ -30,7 +35,4 @@ app.use("/api/products", productRoute);
 app.use("/api/cart", cartRoute);
 //order api
 app.use("/api/orders", orderRoute);
-app.listen(process.env.PORT 
-    || 8000, ()=>{
-    console.log("Backend server is running");
-});
+
