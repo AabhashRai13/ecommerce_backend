33643a1eb25f167843a8831c92ec61f7f3eb88d7
arai <aabhash@pegotec.net>
2022-12-05 22:00:46 +0545

    cart changed

--- models/Cart.js
+++ models/Cart.js
@@ -1,21 +1,25 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
 const CartSchema = new mongoose.Schema(
-  {
-    userId: { type: String, required: true },
-    products: [
-      {
-        productId: {
-          type: String,
+    {
+        userId: {
+            type: String, required: true, unique: true
         },
-        quantity: {
-          type: Number,
-          default: 1,
-        },
-      },
-    ],
-  },
-  { timestamps: true }
+        products: [
+            {
+                productId:{
+                    type:String
+                },
+                quantity:{
+                    type: Number,
+                    default: 1,
+                }
+            }
+        ],
+      
+
+    },
+    { timestamps: true }
 );
 
 module.exports = mongoose.model("Cart", CartSchema);
--- routes/cart.js
+++ routes/cart.js
@@ -1,70 +1,73 @@
 const Cart = require("../models/Cart");
-const {
-  verifyToken,
-  verifyTokenAndAuthorization,
-  verifyTokenAndAdmin,
-} = require("./verifyToken");
+const {verifyToken, verifyTokenAndAuthorizarion, verifyTokenAndAdmin } = require("./verifyToken");
 
 const router = require("express").Router();
 
-//CREATE
+
+//Create Cart
 
 router.post("/", verifyToken, async (req, res) => {
-  const newCart = new Cart(req.body);
+    const newCart = new Cart(req.body)
+    try {
+        const savedCart = await newCart.save();
+        res.status(200).json(savedCart);
+    } catch (error) {
+        res.status(500).json(error);
 
-  try {
-    const savedCart = await newCart.save();
-    res.status(200).json(savedCart);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+    }
 });
 
-//UPDATE
-router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
-  try {
-    const updatedCart = await Cart.findByIdAndUpdate(
-      req.params.id,
-      {
-        $set: req.body,
-      },
-      { new: true }
-    );
-    res.status(200).json(updatedCart);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+//update Cart
+router.put("/:userId/:id", verifyTokenAndAuthorizarion, async (req, res) => {
+
+    try {
+        const updatedCart = await Cart.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        }, { new: true });
+        res.status(200).json(updatedCart);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 //DELETE
-router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
-  try {
-    await Cart.findByIdAndDelete(req.params.id);
-    res.status(200).json("Cart has been deleted...");
-  } catch (err) {
-    res.status(500).json(err);
-  }
+
+router.delete("/:userId/:id", verifyTokenAndAuthorizarion, async (req, res) => {
+    try {
+        await Cart.findByIdAndDelete(req.params.id);
+        res.status(200).json("Cart has been deleted....")
+    }
+    catch (err) {
+        res.status(500).json(err)
+    }
 });
 
-//GET USER CART
-router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
-  try {
-    const cart = await Cart.findOne({ userId: req.params.userId });
-    res.status(200).json(cart);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+//GET user Cart
+
+router.get("/find/:userId",verifyTokenAndAuthorizarion, async (req, res) => {
+    console.log(`request parameters ${req.params.userId}`)
+
+    try {
+        const cart = await Cart.findOne({ userId: req.params.userId });
+        res.status(200).json(cart);
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
 });
 
-// //GET ALL
+//GET all Cart
 
 router.get("/", verifyTokenAndAdmin, async (req, res) => {
-  try {
-    const carts = await Cart.find();
-    res.status(200).json(carts);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+
+    try {
+        const carts = await Cart.find();
+        res.status(200).json(carts);
+    }
+    catch (err) {
+        res.status(500).json(err)
+    }
 });
 
-module.exports = router;
+
+module.exports = router
